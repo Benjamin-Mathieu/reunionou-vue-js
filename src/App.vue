160ba6cd0253b06753a83a4d46386d9d3e9fdda1
Benjamin <ben88200@gmail.com>
2021-03-23 03:15:04 +0100

    Skeleton

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
+  margin: 0;
+  font-family: Roboto, Geneva, Tahoma, sans-serif;
+}
+
+</style>
