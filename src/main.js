e7720d96c659bc901a6bd268434178c486d952ce
Benjamin <ben88200@gmail.com>
2021-03-23 11:27:58 +0100

    Ajout Bulma

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+require('@/assets/main.scss');
 
-Vue.config.productionTip = false
+// Connexion à l'api
+window.api = axios.create({
+  baseURL: "https://allweb.fun/coop/api"
+});
+
+Vue.config.productionTip = false;
+Vue.prototype.$bus = new Vue();
 
 new Vue({
   router,
