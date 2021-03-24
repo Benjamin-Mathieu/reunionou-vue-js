2ac7689f91cdd9808b281ff53f177f433512e9a8
Benjamin <ben88200@gmail.com>
2021-03-24 18:17:21 +0100

    Stockage des évènements dans le store

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
   <div id="app">
+    <Sidebar/>
     <router-view/>
   </div>
 </template>
 
-// <script>
-// import Sidebar from '@/components/Sidebar.vue';
-// export default {
-//   components: {
-//     Sidebar
-//   },
-//   name: 'Home',
-  
-// }
-// </script>
-
+<script>
+import Sidebar from '@/components/Sidebar.vue';
+export default {
+    components: {
+        Sidebar
+    },
+    mounted() {
+      this.getEvents();
+      this.$bus.$on('getEvents', this.getEvents);
+    },
+    methods: {
+      getEvents() {
+        api.get("/events").then(response => {
+                this.$store.commit("setEvents", response.data.events);
+            }).catch(error => {
+                alert(error.response.data.message)
+            })
+      }
+    }
+  }
+</script>
 
 <style>
 * {
