8854b88c7312a1a8f0d9b631530a3c21a5482af4
Benjamin <ben88200@gmail.com>
2021-03-25 10:15:28 +0100

    Modification chemin route home

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,7 +2,7 @@
     <div class="sidebar">
         <nav class="navbar" role="navigation" aria-label="dropdown navigation">
             <a class="navbar-item">
-                <router-link to="/home"><h1>Reunionou</h1></router-link>
+                <router-link to="/"><h1>Reunionou</h1></router-link>
             </a>
             <a class="navbar-item">
                 <router-link to="/profil">Profil</router-link>
