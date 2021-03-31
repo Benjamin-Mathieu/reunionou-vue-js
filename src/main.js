bb2943277cb5fd4201ccf684fe2e0fcd37ed70aa
Benjamin <ben88200@gmail.com>
2021-03-31 17:08:18 +0200

    test api

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ require('@/assets/main.scss');
 
 // Connexion à l'api
 window.api = axios.create({
-  baseURL: "https://docketu.iutnc.univ-lorraine.fr",
+  baseURL: "https://docketu.iutnc.univ-lorraine.fr:14001",
   // withCredentials: true
 });
 window.api_adress = axios.create({
